47425cb4c22a3b8e09a42221804d4af636bfbc9f
Achrafsadeq <achrafsadeqq@gmail.com>
2025-02-17 14:43:30 -0800

    Create endpoints for managing City objects (GET, POST, PUT, DELETE).

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Initialize the views module"""
+"""
+Initialize the views module for the API
+"""
 from flask import Blueprint
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+from api.v1.views.index import *
 
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
