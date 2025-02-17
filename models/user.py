2e600360e0e6905fa97af53ce9b805cbffb72da9
Achrafsadeq <achrafsadeqq@gmail.com>
2025-02-17 09:47:51 -0800

    A README.md file at the root of the project folder.

---
+++ models/user.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""User module for the AirBnB clone project."""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+
+class User(BaseModel, Base):
+    """User class for representing users in the AirBnB clone.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    __tablename__ = 'users'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
